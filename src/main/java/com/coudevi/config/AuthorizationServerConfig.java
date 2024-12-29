576928c214680a8eee23e8c27e2ac5d8dc848975
Victor Fernández <victor.fernandez1006@gmail.com>
2024-12-29 08:22:13 -0500

    feature - add basic configuration Client Credentials

--- src/main/java/com/coudevi/config/AuthorizationServerConfig.java
+++ src/main/java/com/coudevi/config/AuthorizationServerConfig.java
@@ -2,14 +2,20 @@ package com.coudevi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Configuration
@@ -21,14 +27,30 @@ public class AuthorizationServerConfig {
     }
 
     @Bean
-    public InMemoryRegisteredClientRepository registeredClientRepository(){
+    public AuthorizationServerSettings authorizationServerSettings(){
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:9000")// URL del issuer
+                .build();
+    }
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("my-client")
-                .clientSecret("{noop}my-secret")
+                .clientId("my-client") // Este debe coincidir
+                .clientSecret("{noop}my-secret") // Este también debe coincidir
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                 .scope("read")
                 .scope("write")
                 .build();
+
         return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+        return context -> {
+            if (context.getTokenType().getValue().equals("access_token")) {
+                // Agrega los scopes al JWT
+                context.getClaims().claim("scope", String.join(" ", context.getAuthorizedScopes()));
+            }
+        };
     }
 }
